0c62d21003fae1413d2ca865d3eafaa0d34511a7
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-04-27 15:00:53 +0300

    vercel+github

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/gShield/">
+  <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/gShield/' : '/'}>
     <StrictMode>
       <App />
     </StrictMode>
