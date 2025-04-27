cf295d372148aa2090a8cad523fe791e94f08e36
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-04-27 14:02:01 +0300

    Updates

---
+++ assets/Our-Standards-Cexxm0OA.js
@@ -0,0 +1,1 @@
+const s="/gShield/aboutSec/Our-Vision.webp",o="/gShield/aboutSec/Our-Message.webp",e="/gShield/aboutSec/Our-Mission.webp",a="/gShield/aboutSec/Our-Standards.webp";export{e as a,a as b,o as c,s as o};
