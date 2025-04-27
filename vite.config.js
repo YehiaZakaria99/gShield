5ee0f50fbbcc5e1bd719c7ac3153d71f5b87f7ee
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-04-27 15:17:11 +0300

    teeeessttt

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/gShield/' : '/',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    base: mode === 'production' ? '/gShield/' : '/',
+  };
 });
