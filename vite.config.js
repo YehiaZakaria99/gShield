d685f00076ec7855bdfab54307af944d8de886a5
YehiaZakaria99 <yehiazakaria16199@gmail.com>
2025-04-27 15:03:04 +0300

    vercel+github02

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/gShield/'
-})
+  base: process.env.NODE_ENV === "production" ? "/gShield/" : "/",
+});
